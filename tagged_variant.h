5cb3e0497a37a81b5be3b32aba91ec246a37773c
Yuriy Kulinchenko <ykulinchenko@gmail.com>
2026-06-24 15:23:11 +0100

    Added requirement that duplicate tags cannot be used in a single tagged_variant.

--- tagged_variant.h
+++ tagged_variant.h
@@ -28,7 +28,15 @@ namespace impl_t_var {
     template<typename T, typename Vector>
     using cons = typename cons_class<T, Vector>::vector;
 
-    // 'safe_entries' enforces that all entry tags come from the same enum
+    // 'only_entries' enforces that all entries passed are indeed entry types:
+
+    template<typename T>
+    struct is_entry : std::false_type {};
+
+    template<Enum auto tag, typename T>
+    struct is_entry<entry<tag, T>> : std::true_type {};
+
+    // 'safe_entries' enforces that all entry tags come from the same enum:
 
     template<typename... Entries>
     struct safe_entries_class;
@@ -45,11 +53,40 @@ namespace impl_t_var {
 
     template<Enum auto t1, Enum auto t2, typename T1, typename  T2, typename... Entries>
     struct safe_entries_class<entry<t1, T1>, entry<t2, T2>, Entries...> {
-        static constexpr bool value = std::same_as<decltype(t1), decltype(t2)> &&
-            safe_entries_class<entry<t2,T2>, Entries...>::value;
+        static constexpr bool value = std::same_as<decltype(t1), decltype(t2)>
+            && safe_entries_class<entry<t2,T2>, Entries...>::value;
     };
 
-    // convert Entries -> std::variant
+    // 'duplicate_tags' checks whether a duplicate tag exists - this shouldn't be allowed:
+
+    template<Enum auto tag, typename... Entries>
+    struct tag_occurrence;
+
+    template<Enum auto tag>
+    struct tag_occurrence<tag> {
+        static constexpr bool value = false;
+    };
+
+    template<Enum auto tag, Enum auto other, typename T, typename... Rest>
+    struct tag_occurrence<tag, entry<other, T>, Rest...> {
+        static constexpr bool value = (tag == other) || tag_occurrence<tag, Rest...>::value;
+    };
+
+    template<typename... Entries>
+    struct duplicate_tags_class;
+
+    template<>
+    struct duplicate_tags_class<> {
+        static constexpr bool value = false;
+    };
+
+    template<Enum auto tag, typename T, typename... Rest>
+    struct duplicate_tags_class<entry<tag, T>, Rest...> {
+        static constexpr bool value = tag_occurrence<tag, Rest...>::value
+            || duplicate_tags_class<Rest...>::value;
+    };
+
+    // Convert 'Entries' to a corresponding variant:
 
     template<typename... Entries>
     struct entries_to_vector;
@@ -74,16 +111,21 @@ namespace impl_t_var {
 
 }
 
+template<typename... Entries>
+concept only_entries = (impl_t_var::is_entry<Entries>::value && ...);
 
 template<typename... Entries>
 concept safe_entries = impl_t_var::safe_entries_class<Entries...>::value;
 
 template<typename... Entries>
+concept duplicate_tags = impl_t_var::duplicate_tags_class<Entries...>::value;
+
+template<typename... Entries>
 using entries_to_variant = typename impl_t_var::vector_to_variant_class<
     typename impl_t_var::entries_to_vector<Entries...>::vector>::type;
 
-
-template<typename... Entries> requires safe_entries<Entries...>
+template<typename... Entries>
+requires only_entries<Entries...> && safe_entries<Entries...> && !duplicate_tags<Entries...>
 class tagged_variant {
 public:
     using variant = entries_to_variant<Entries...>;
