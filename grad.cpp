a7f248b054558f6600c56256cca4a4e982ba3c2f
Yuriy Kulinchenko <ykulinchenko@gmail.com>
2026-06-30 10:42:32 +0100

    Cleaned up grad file using fresh_var method, and removal of most namespace navigation.

--- grad.cpp
+++ grad.cpp
@@ -29,23 +29,24 @@ The current implementation of grad will not be handling nested expressions, howe
 amenable to this extension.
 */
 
-jax::expression grad(const jax::expression& expr) {
+using namespace jax;
+
+expression grad(const expression& expr) {
     // For now, take the seed to be f32:
-    using namespace jax;
     auto instance = grad_class{expr};
     return instance.find_grad(value{array_t::build_fill(type_t{type_enum::F32}, 1)});
 }
 
-grad_class::grad_class(const jax::expression& expr): input_expr(expr) {
+grad_class::grad_class(const expression& expr): input_expr(expr) {
     output_expr.var_id = expr.var_id;
 }
 
-jax::var_t grad_class::fresh_var(jax::type_t type) {
-    return jax::var_t{output_expr.new_var_id(), std::move(type)};
+var_t grad_class::fresh_var(type_t type) {
+    return var_t{output_expr.new_var_id(), std::move(type)};
 }
 
 
-void grad_class::introduce_adjoint(const jax::var_t& var) {
+void grad_class::introduce_adjoint(const var_t& var) {
     /*
     For each intermediate value in the calculation, an adjoint
     has to be produced which is added to. A simple way to introduce
@@ -64,7 +65,7 @@ void grad_class::introduce_adjoint(const jax::var_t& var) {
     active_adjoint_map[var.get_id()] = false;
 }
 
-jax::value *grad_class::get_adjoint(const jax::var_t& var) {
+value *grad_class::get_adjoint(const var_t& var) {
     if (auto it = variable_adjoint_map.find(var.get_id());
         it != variable_adjoint_map.end()) {
         return &it->second;
@@ -72,16 +73,16 @@ jax::value *grad_class::get_adjoint(const jax::var_t& var) {
     return nullptr;
 }
 
-bool grad_class::adjoint_is_active(jax::var_t var, bool apply_update) {
+bool grad_class::adjoint_is_active(var_t var, bool apply_update) {
     auto it = active_adjoint_map.find(var.get_id());
     if (it->second) return true;
     if (apply_update) it->second = true;
     return false;
 }
 
-void grad_class::update_adjoint(const jax::var_t& input_adj_var, const jax::value& product_val) {
+void grad_class::update_adjoint(const var_t& input_adj_var, const value& product_val) {
     // First, check if product is zero:
-    if (product_val.is<jax::array_t>()) {
+    if (product_val.is<array_t>()) {
         if (product_val.get_array().has_single_value(0)) {
             return;
         }
@@ -89,39 +90,39 @@ void grad_class::update_adjoint(const jax::var_t& input_adj_var, const jax::valu
 
     // Case 1: input_adj does not yet exist:
     if (!adjoint_is_active(input_adj_var)) {
-        variable_adjoint_map.insert_or_assign(input_adj_var.get_id(), jax::value{product_val});
+        variable_adjoint_map.insert_or_assign(input_adj_var.get_id(), value{product_val});
     } else {
         // Case 2: it does exist - sum is required:
-        auto sum_var = jax::var_t{output_expr.new_var_id(), product_val.get_type()};
+        auto sum_var = var_t{output_expr.new_var_id(), product_val.get_type()};
         const auto& input_adj = *get_adjoint(input_adj_var);
 
         output_expr.equations.emplace_back(
             std::vector{product_val, input_adj},
             std::vector{sum_var},
-            jax::primitive_op::ADD
+            primitive_op::ADD
         );
 
         // Carry through the updated adjoint:
-        variable_adjoint_map.insert_or_assign(input_adj_var.get_id(), jax::value{sum_var});
+        variable_adjoint_map.insert_or_assign(input_adj_var.get_id(), value{sum_var});
 
     }
 }
 
 void grad_class::update_adjoint(
-    const jax::var_t& input_adj_var,
-    const jax::value& f_prime_val,
-    const jax::value& adjoint_val) {
+    const var_t& input_adj_var,
+    const value& f_prime_val,
+    const value& adjoint_val) {
 
-    bool f_prime_is_array = f_prime_val.is<jax::array_t>();
-    bool adjoint_is_array = adjoint_val.is<jax::array_t>();
+    bool f_prime_is_array = f_prime_val.is<array_t>();
+    bool adjoint_is_array = adjoint_val.is<array_t>();
 
     if (f_prime_is_array && adjoint_is_array) {
         // Fold constant:
-        jax::array_t product = f_prime_val.get_array() * adjoint_val.get_array();
+        array_t product = f_prime_val.get_array() * adjoint_val.get_array();
         if (product.has_single_value(0)) return;
-        update_adjoint(input_adj_var, jax::value{std::move(product)});
+        update_adjoint(input_adj_var, value{std::move(product)});
     } else if (f_prime_is_array) {
-        const jax::array_t& f_prime = f_prime_val.get_array();
+        const array_t& f_prime = f_prime_val.get_array();
         if (f_prime.has_single_value(0)) {
             // If f_prime is 0, return early
             return;
@@ -135,16 +136,16 @@ void grad_class::update_adjoint(
             output_expr.equations.emplace_back(
                 std::vector{adjoint_val},
                 std::vector{negated_var},
-                jax::primitive_op::NEG
+                primitive_op::NEG
             );
-            update_adjoint(input_adj_var, jax::value{negated_var});
+            update_adjoint(input_adj_var, value{negated_var});
         } else {
             // Otherwise, actually multiply
             update_adjoint(input_adj_var, f_prime_val, adjoint_val);
         }
 
     } else if (adjoint_is_array) {
-        const jax::array_t& adjoint = adjoint_val.get_array();
+        const array_t& adjoint = adjoint_val.get_array();
         if (adjoint.has_single_value(0)) {
             return;
         }
@@ -155,9 +156,9 @@ void grad_class::update_adjoint(
             output_expr.equations.emplace_back(
                 std::vector{f_prime_val},
                 std::vector{negated_var},
-                jax::primitive_op::NEG
+                primitive_op::NEG
             );
-            update_adjoint(input_adj_var, jax::value{negated_var});
+            update_adjoint(input_adj_var, value{negated_var});
         } else {
             update_adjoint(input_adj_var, f_prime_val, adjoint_val);
         }
@@ -168,19 +169,19 @@ void grad_class::update_adjoint(
         output_expr.equations.emplace_back(
             std::vector{f_prime_val, adjoint_val},
             std::vector{product_var},
-            jax::primitive_op::MUL
+            primitive_op::MUL
         );
 
-        update_adjoint(input_adj_var, jax::value{product_var});
+        update_adjoint(input_adj_var, value{product_var});
     }
 }
 
-bool all_inputs_constant(const std::vector<jax::value>& inputs) {
+bool all_inputs_constant(const std::vector<value>& inputs) {
     for (auto& input: inputs) if (input.is<jax::var_t>()) return false;
     return true;
 }
 
-void grad_class::propagate_adjoints(const jax::equation& eq) {
+void grad_class::propagate_adjoints(const equation& eq) {
     // Requires knowledge of output adjoints, and shape of eq.op to dispatch over
     // If an adjoint does not exist, take it to be zero
 
@@ -188,7 +189,7 @@ void grad_class::propagate_adjoints(const jax::equation& eq) {
     if (all_inputs_constant(eq.get_input())) return;
 
     switch (eq.get_op()) {
-        using enum jax::primitive_op;
+        using enum primitive_op;
         case SIN: {
             auto& output_var = eq.get_output(0);
             auto* output_adj = get_adjoint(output_var);
@@ -196,7 +197,7 @@ void grad_class::propagate_adjoints(const jax::equation& eq) {
 
             auto& input_var = eq.get_input(0).get_var();
 
-            auto f_prime_var = jax::var_t{output_expr.new_var_id(), output_var.get_type()};
+            auto f_prime_var = fresh_var(output_var.get_type());
 
             output_expr.equations.emplace_back(
                 std::vector{jax::value{input_var}},
@@ -204,7 +205,7 @@ void grad_class::propagate_adjoints(const jax::equation& eq) {
                 COS
             );
 
-            update_adjoint(input_var, jax::value{f_prime_var}, *output_adj);
+            update_adjoint(input_var, value{f_prime_var}, *output_adj);
 
             break;
         }
@@ -216,23 +217,23 @@ void grad_class::propagate_adjoints(const jax::equation& eq) {
 
             auto& input_var = eq.get_input(0).get_var();
 
-            auto var1 = jax::var_t{output_expr.new_var_id(), output_var.get_type()};
+            auto var1 = fresh_var(output_var.get_type());
 
             output_expr.equations.emplace_back(
-                std::vector{jax::value{input_var}},
+                std::vector{value{input_var}},
                 std::vector{var1},
                 SIN
             );
 
-            auto f_prime_var = jax::var_t{output_expr.new_var_id(), output_var.get_type()};
+            auto f_prime_var = fresh_var(output_var.get_type());
 
             output_expr.equations.emplace_back(
-                std::vector{jax::value{var1}},
+                std::vector{value{var1}},
                 std::vector{f_prime_var},
                 NEG
             );
 
-            update_adjoint(input_var, jax::value{f_prime_var}, *output_adj);
+            update_adjoint(input_var, value{f_prime_var}, *output_adj);
 
             break;
         }
@@ -243,7 +244,7 @@ void grad_class::propagate_adjoints(const jax::equation& eq) {
             if (!output_adj) break;
 
             auto& input_var = eq.get_input(0).get_var();
-            auto f_prime = jax::array_t::build_fill(output_var.get_type(), -1); // -1
+            auto f_prime = array_t::build_fill(output_var.get_type(), -1); // -1
 
             update_adjoint(input_var, jax::value{f_prime}, *output_adj);
 
@@ -263,11 +264,11 @@ void grad_class::propagate_adjoints(const jax::equation& eq) {
             auto& x_val = eq.get_input(0);
             auto& y_val = eq.get_input(1);
 
-            if (x_val.is<jax::var_t>()) {
+            if (x_val.is<var_t>()) {
                 update_adjoint(x_val.get_var(), *output_adj);
             }
 
-            if (y_val.is<jax::var_t>()) {
+            if (y_val.is<var_t>()) {
                 update_adjoint(y_val.get_var(), *output_adj);
             }
             break;
@@ -286,11 +287,11 @@ void grad_class::propagate_adjoints(const jax::equation& eq) {
             auto& x_val = eq.get_input(0);
             auto& y_val = eq.get_input(1);
 
-            if (x_val.is<jax::var_t>()) {
+            if (x_val.is<var_t>()) {
                 update_adjoint(x_val.get_var(), y_val, *output_adj);
             }
 
-            if (y_val.is<jax::var_t>()) {
+            if (y_val.is<var_t>()) {
                 update_adjoint(y_val.get_var(), x_val, *output_adj);
             }
 
@@ -303,16 +304,16 @@ void grad_class::propagate_adjoints(const jax::equation& eq) {
     }
 }
 
-jax::expression grad_class::find_grad(jax::value seed) {
+expression grad_class::find_grad(value seed) {
     if (input_expr.outvals.size() != 1) {
         throw formatted_error("Error: expected 1 output, received {}", input_expr.outvals.size());
     }
 
-    if (input_expr.outvals[0].is<jax::array_t>()) {
+    if (input_expr.outvals[0].is<array_t>()) {
         throw formatted_error("Error: expected output to be variable, received array");
     }
 
-    const jax::var_t& output_var = input_expr.outvals[0].get_var();
+    const var_t& output_var = input_expr.outvals[0].get_var();
 
     if (output_var.get_type().get_dimension().size() != 0) {
         throw formatted_error("Error: shape of output variable must be scalar");
@@ -347,13 +348,13 @@ jax::expression grad_class::find_grad(jax::value seed) {
     // Populate outputs:
 
     for (auto& var: input_expr.invars) {
-        jax::value* val = get_adjoint(var);
+        value* val = get_adjoint(var);
         if (val) {
             // The adjoint exists:
             output_expr.outvals.push_back(*val);
         } else {
             // The adjoint does not exist - replace it with 0:
-            output_expr.outvals.push_back(jax::value{jax::array_t::build_fill(var.get_type(), 0)});
+            output_expr.outvals.push_back(value{array_t::build_fill(var.get_type(), 0)});
         }
     }
 
